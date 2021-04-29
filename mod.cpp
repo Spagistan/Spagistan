e5eb8d5e24b934bbe3f03c5a64babffc707d4f7a
cruelnoise <cruelnoise@gmail.com>
2021-04-29 18:15:07 -0400

    added mod.cpp

---
+++ mod.cpp
@@ -0,0 +1,12 @@
+name = "Spagistan Armed Forces Mod";
+tooltipOwned="Spagistan Armed Forces Mod";
+picture = "spagistan\addons\spagistan_main\data\spagistan_logo_ca.paa";
+logoSmall = "spagistan\addons\spagistan_main\data\spagistan_logo_ca.paa";
+logo = "spagistan\addons\spagistan_main\data\spagistan_logo_ca.paa";
+logoOver = "spagistan\addons\spagistan_main\data\spagistan_logo_ca.paa";
+hideName = 0;
+hidePicture = 0;
+actionName = "Website";
+action = "https://spagistan.com";
+description = "Spagistan Armed Forces Mod";
+overview = "Spagistan Armed Forces Mod";
